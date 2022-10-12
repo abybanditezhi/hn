0019b02c4c802c36745049154335e15a8d4e51e1
KromberK123 <a37507687@gmail.com>
2022-10-12 20:53:38 +0700

    urok 4, 12 oct

---
+++ urok 4/dota.py
@@ -0,0 +1,93 @@
+# a = input('Введите имя: ').capitalize()
+# b = input('Введите фамилию: ').capitalize()
+# c = input('введите отчество: ').capitalize()
+# # .капитализе - первая с большой
+# #два варика
+# print(b, a[0] + '.', c[0] + '.')
+# print(f'{b} {a[0]}. {c[0]}.')
+
+# x = 'abytalabashyneba'.count('a')
+# print(x)
+
+# a = 'hello world'
+# x = a.split(' ')
+# # x.remove('hello') - значение
+# # x.pop(0) - индекс
+# x.pop(-1)
+# print(x)
+
+# a = input()
+# b = input('find: ')
+# c = input('replace: ')
+# a.replace(b, c)
+
+# x = input()
+# print(x.replace('ё', 'е'))
+
+
+
+# dict = {
+#     'а': 'a',
+#     'б': 'b',
+#     'в': 'v',
+#     'г': 'g',
+#     'д': 'd',
+#     'е': 'e',
+#     'ё': 'yo',
+#     'ж': 'zh',
+#     'з': 'z',
+#     'и': 'i',
+#     'й': 'i',
+#     'к': 'k',
+#     'л': 'l',
+#     'м': 'm',
+#     'н': 'n',
+#     'о': 'o',
+#     'п': 'p',
+#     'р': 'r',
+#     'с': 's',
+#     'т': 't',
+#     'у': 'u',
+#     'ф': 'f',
+#     'х': 'h',
+#     'ц': 'ts',
+#     'ш': 'sh',
+#     'ъ': 'sh',
+#     'ы': 'i',
+#     'ь': '`',
+#     'э': 'e',
+#     'ю': 'u',
+#     'я': 'ya',
+#     ' ': ' ',
+#     ',': ',',
+# }
+#
+# x = input()
+# trans = ''
+# for bukva in x:
+#     dict[bukva]
+#     trans = trans + dict[bukva]
+# print(trans)
+
+# x = input('Введите вашу почту: ')
+# temp = x.split('@')
+# print(f'login: {temp[0]} domain: {temp[1]}' )
+
+# x = 5
+# if x == 5:
+#     print('ай сау броооооо')
+
+# x = int(input())
+# if x > 0:
+#     print('положительное')
+# elif x < 0:
+#     print('отрицательное')
+# else:
+#     print('0')
+
+# year = int(input())
+# if year % 4 == 0 and year % 400 == 0 or year % 100 == 0 :
+#     print('год високосный')
+# else:
+#     print('(((((((((((')
+
